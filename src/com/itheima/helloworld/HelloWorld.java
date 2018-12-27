f88a868742da127ed32975d0de40acc9993e1022
GGC <945285397@qq.com>
2018-12-27 09:48:06 +0800

    提交一次

---
+++ src/com/itheima/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hw = "HelloWorld!";
+		System.out.println(hw);
+	}
+
+}
